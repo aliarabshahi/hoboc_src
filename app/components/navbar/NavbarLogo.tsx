e72f5b3cf17761285cbec271460f0e61a74f6746
user <user@users-MacBook-Pro.local>
2025-07-05 13:44:24 +0330

    adding link to logo

--- app/components/navbar/NavbarLogo.tsx
+++ app/components/navbar/NavbarLogo.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavbarLogo() {
   return (
-    // <div>
-    //   <Image
-    //     src="/logo.png"
-    //     alt="Hoboc Logo"
-    //     width={32}  // عرض مورد نظر
-    //     height={32} // ارتفاع مورد نظر
-    //     className="h-8 w-auto"
-    //     priority // برای تصاویر مهم در بالای صفحه
-    //   />
-    // </div>
-    <div className="text-lg md:text-2xl font-bold text-hoboc">HOBOC</div>
+    <Link href="/">
+      {/* 
+      <div>
+        <Image
+          src="/logo.png"
+          alt="Hoboc Logo"
+          width={32}  // عرض مورد نظر
+          height={32} // ارتفاع مورد نظر
+          className="h-8 w-auto"
+          priority // برای تصاویر مهم در بالای صفحه
+        />
+      </div> 
+      */}
+      <div className="text-lg md:text-2xl font-bold text-hoboc cursor-pointer">HOBOC</div>
+    </Link>
   );
 }
