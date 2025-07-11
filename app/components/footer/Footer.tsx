ebd1d42e5c1f11a1ac3efa9c1c0b61ad3e81e08b
user <user@users-MacBook-Pro.local>
2025-07-11 10:00:52 +0330

    fix footer bug

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -4,9 +4,9 @@ import FooterSocial from "./FooterSocial";
 
 export default function Footer() {
   return (
-    <footer className="footer bg-base-300 text-base-content py-10 px-60 border-t border-gray-300 bg-white" dir="rtl">
+    <footer className="footer bg-base-300 text-base-content py-10 px-4 sm:px-60 border-t border-gray-300 bg-white" dir="rtl">
       <div className="container mx-auto">
-        <div className="flex flex-col md:flex-row justify-between items-center gap-8 w-full">
+        <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-8 w-full">
           <FooterSocial />
           <FooterNavbar />
         </div>
