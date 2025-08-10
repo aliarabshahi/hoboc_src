4c3a33000f68309a950e20a63977aba8c270c082
user <user@users-MacBook-Pro.local>
2025-08-10 14:52:56 +0330

    fix vision iamge bug

--- app/components/vision/VisionImage.tsx
+++ app/components/vision/VisionImage.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function VisionImage({ className }: { className?: string }) {
   return (
-    <div className={`${className ?? ""}`}>
+    <div
+      className={`${className ?? ""} relative w-full min-h-[300px] flex items-center justify-center`}
+    >
       <Image
         src="/main/Education-bro (5).svg"
         alt="Vision Image"
         width={1500}
         height={1500}
-        className="object-cover w-full h-full"
+        className="object-contain w-full h-auto"
       />
     </div>
   );
